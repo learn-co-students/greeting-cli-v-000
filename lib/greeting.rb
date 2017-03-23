ed1e581758af5634221899fc1d88cb96f843b2af
Will Sauer <will@heueso.com>
2017-03-23 19:31:21 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,8 @@
-# code the #greeting method here!
+# the greeting method takes a name from the user and
+#puts a personalized greeting
+
+def greeting (name = "Intrepid Explorer")
+  puts "Hello #{name}. It's nice to meet you.
+   If the gods favor you, 1000 pink sparros will
+   fly in your path today."
+end
