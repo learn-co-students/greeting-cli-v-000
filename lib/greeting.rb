d022fb55388943c882912fc8fedbe0cdcae0e5c1
Min-Si Wang <minsiwang04@gmail.com>
2017-08-27 11:17:23 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,11 @@
 
-"Hi! I'm #{name}, what's your name?"
+puts  "Hi! I'm Hal, what's your name?"
+
 
 def greeting(name)
   puts "Hi! I'm #{name}, what's your name?"
 end
+
+name = gets.strip
 
 greeting(name)
