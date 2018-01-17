b6873497bd8383cd7884b1de6edd9efbb21a18d2
Hiroki Kato <hkato@sas.upenn.edu>
2018-01-17 19:41:07 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,4 @@
-# code the #greeting method here!
+# code the #greeting method here!  def greeting (name)
+def greeting(name = "Full Stack Programmer")
+  puts "Hello #{(name)}. It's nice to meet you."
+end
